3d928d34f36cdc14d0bfaa7c81e4de9a0b4dccd1
a2447742618 <2447742618@qq.com>
2021-06-01 20:18:26 +0800

    增加list模块列出节点记录的功能 修改了new-branch获得当前分支的路径错误 解决了init-db获得本地时间的内存释放问题

--- src/Database/init-db.cpp
+++ src/Database/init-db.cpp
@@ -172,8 +172,10 @@ void init_db::do_init() {
 
     //创建根节点
     char tmp_time[100];
+    auto tmpp = database::getCurrentTimeChar();
+    strcpy(tmp_time, tmpp);
+    free(tmpp);
 
-    strcpy(tmp_time, database::getCurrentTimeChar());
 
     char tmp_sql[1000];
 
@@ -204,7 +206,9 @@ void init_db::do_init() {
 
 
     //连接主分支和根节点
-    strcpy(tmp_time, database::getCurrentTimeChar());
+    tmpp = database::getCurrentTimeChar();
+    strcpy(tmp_time, tmpp);
+    free(tmpp);
 
     sprintf(tmp_sql,
             "INSERT INTO Node2Branch (ID,Node,Branch,CreatedDateTime) VALUES (NULL, (SELECT SHA FROM Node WHERE SHA = '000000'), (SELECT ID FROM Branch WHERE ID = 1),'%s')",
--- src/module_list.cpp
+++ src/module_list.cpp
@@ -5,15 +5,43 @@
 #include "module_list.h"
 #include<sqlite3.h>
 #include<iostream>
+#include<vector>
+#include<cstring>
+#include "Database/database.h"
 
 using namespace std;
 
-static int callback(void *NotUsed, int cnt, char **pValue, char **pName)//用于输出branch表所有信息的回调函数
+struct branch1 {
+    int id;
+    char name[100], root[100], head[100], created[100], updated[100];
+};
+vector<branch1> sav;//保存分支信息
+
+static int callback(void *NotUsed, int cnt, char **pValue, char **pName)//用于获得branch表所有信息的回调函数
 {
-    for (int i = 0; i < cnt; i++) {
-        cout << pName[i] << "=" << (pValue[i] ? pValue[i] : "NULL") << endl;
-    }
-    cout << endl;
+    branch1 tmp;
+    tmp.id = atoi(pValue[0]);
+    strcpy(tmp.name, pValue[1]);
+    strcpy(tmp.root, pValue[2]);
+    strcpy(tmp.head, pValue[3]);
+    strcpy(tmp.created, pValue[4]);
+    strcpy(tmp.updated, pValue[5]);
+    sav.push_back(tmp);
+    return 0;
+}
+
+char head_node[100], root_node[100];
+
+static int get_node(void *NotUsed, int cnt, char **pValue, char **pName)//输出节点信息并向上更新节点的回调函数
+{
+    cout << pValue[0] << ": " << pValue[3] << " " << pValue[1] << endl;
+    strcpy(head_node, pValue[2]);
+    return 0;
+}
+
+static int out_root(void *NotUsed, int cnt, char **pValue, char **pName)//输出根节点信息的回调函数
+{
+    cout << pValue[0] << ": " << pValue[3] << " " << pValue[1] << endl << endl;
     return 0;
 }
 
@@ -21,7 +49,7 @@ void module_list::list() {
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
-    char *sql;
+    char sql[500];
 
     //打开数据库
     rc = sqlite3_open(".simple-scm/simple-scm.db", &db);
@@ -32,15 +60,44 @@ void module_list::list() {
         clog << "[INFO]数据库打开成功！" << endl;
     }
 
-
-    //获得branch表中所有信息并输出
-    sql = "SELECT * FROM Branch";
+    //获得branch表中所有信息
+    sprintf(sql, "SELECT * FROM Branch");
     rc = sqlite3_exec(db, sql, callback, NULL, &zErrMsg);
 
     if (rc != SQLITE_OK) {
         cerr << "[ERROR]发生错误：" << zErrMsg << endl;
         exit(1);
     }
+
+    //遍历分支，输出分支信息后输出节点记录
+    for (auto p:sav) {
+        cout << "ID=" << p.id << endl << "Name=" << p.name << endl << "BranchRoot=" << p.root << endl
+             << "BranchHead=" << p.head << endl << "CreatedDateTime=" << p.created << endl
+             << "UpdatedDateTime=" << p.updated << endl << endl << p.name << "分支提交记录为：" << endl;
+
+        strcpy(head_node, p.head);
+        strcpy(root_node, p.root);
+        //边输出节点信息边向上更新，直到根节点为止
+        while (strcmp(head_node, root_node) != 0) {
+            sprintf(sql, "SELECT * FROM Node WHERE SHA='%s'", head_node);
+            rc = sqlite3_exec(db, sql, get_node, NULL, &zErrMsg);
+
+            if (rc != SQLITE_OK) {
+                cerr << "[ERROR]发生错误：" << zErrMsg << endl;
+                exit(1);
+            }
+        }
+
+        //输出根节点信息
+        sprintf(sql, "SELECT * FROM Node WHERE SHA='%s'", root_node);
+        rc = sqlite3_exec(db, sql, out_root, NULL, &zErrMsg);
+
+        if (rc != SQLITE_OK) {
+            cerr << "[ERROR]发生错误：" << zErrMsg << endl;
+            exit(1);
+        }
+    }
+
     sqlite3_close(db);
 }
 
