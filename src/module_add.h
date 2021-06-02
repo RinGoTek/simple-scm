794b6ea3eb82859762ea81f6cc1661375df9df5c
windsoul <1009427507@qq.com>
2021-06-02 20:07:01 +0800

    Merge branch 'dev' of https://github.com/We-are-ACMen/simple-scm into dev
    # Conflicts:
    #	CMakeLists.txt

---
+++ src/module_add.h
@@ -0,0 +1,15 @@
+//
+// Created by a2447742618 on 2021/6/1.
+//
+
+#ifndef SIMPLE_SCM_MODULE_ADD_H
+#define SIMPLE_SCM_MODULE_ADD_H
+
+
+class module_add {
+public:
+    void add(char *);
+};
+
+
+#endif //SIMPLE_SCM_MODULE_ADD_H
