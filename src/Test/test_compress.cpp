30dc459795a6885048acdefa40abf0bb078a36ea
login <fslongjiN@vip.qq.com>
2021-06-22 17:48:09 +0800

    1`测试压缩单个文件

--- src/Test/test_compress.cpp
+++ src/Test/test_compress.cpp
@@ -3,8 +3,21 @@
 //
 
 #include "test_compress.h"
+#include"../Database/Compress.h"
+#include<iostream>
+#include <ctime>
+using namespace std;
 
 void test_compress::test_compress_one_file() {
+    cout<<"请输入文件路径: ";
+    string path;
+    cin>>path;
+    Compress tmp;
+    clock_t start_time = clock();
+    tmp.compress(path);
+
+    cout<<"压缩完成！耗时："<<1.0*(clock()-start_time)/1000.0<<"ms"<<endl;
+
 
 }
 
