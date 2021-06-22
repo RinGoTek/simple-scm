d75ee445132641548e341539f5cdf158f91ed0db
login <fslongjiN@vip.qq.com>
2021-06-22 16:27:25 +0800

    1`改变了database的代码

--- src/Database/database.cpp
+++ src/Database/database.cpp
@@ -31,10 +31,8 @@ tm *database::getCurrentTime() {
     return t;
 }
 
-char *database::getCurrentTimeChar() {
-    tm *tm_local = getCurrentTime();
-    auto t = tm_local;
-
+char* format_string(tm* t)
+{
     stringstream fmt;
 
     fmt << t->tm_year + 1900 << '-' << setw(2) << setfill('0') << t->tm_mon + 1 << '-' << setw(2) << setfill('0')
@@ -48,11 +46,20 @@ char *database::getCurrentTimeChar() {
     return res;
 }
 
+
+char *database::getCurrentTimeChar() {
+    tm *tm_local = getCurrentTime();
+    auto t = tm_local;
+    return format_string(t);
+
+}
+
 char *database::getTimeChar(time_t tt) {
 
     tt = tt + 8 * 3600;  // transform the time zone
     tm *t = gmtime(&tt);
-
+    return format_string(t);
+    /*
     stringstream fmt;
 
     fmt << t->tm_year + 1900 << '-' << setw(2) << setfill('0') << t->tm_mon + 1 << '-' << setw(2) << setfill('0')
@@ -64,5 +71,5 @@ char *database::getTimeChar(time_t tt) {
     char *res = new char[str.size() + 1];
     strcpy(res, str.c_str());
     return res;
-
+    */
 }
