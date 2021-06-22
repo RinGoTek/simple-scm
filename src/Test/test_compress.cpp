81fee17b9c7e047afb112a85800dacfff4836b06
login <fslongjiN@vip.qq.com>
2021-06-22 16:06:56 +0800

    1`增加了测试模块

---
+++ src/Test/test_compress.cpp
@@ -0,0 +1,5 @@
+//
+// Created by login on 2021/6/22.
+//
+
+#include "test_compress.h"
---
+++ src/Test/test_compress.h
@@ -0,0 +1,14 @@
+//
+// Created by login on 2021/6/22.
+//
+
+#ifndef SIMPLE_SCM_TEST_COMPRESS_H
+#define SIMPLE_SCM_TEST_COMPRESS_H
+
+
+class test_compress {
+
+};
+
+
+#endif //SIMPLE_SCM_TEST_COMPRESS_H
